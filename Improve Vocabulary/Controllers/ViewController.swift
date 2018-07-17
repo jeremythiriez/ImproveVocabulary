0748144d1257d3ea7947b0d81ab298c66abdca64
jeremy thiriez <jeremy.thiriez@gmail.com>
2018-07-17 19:21:40 +0200

    init commit

---
+++ Improve Vocabulary/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Improve Vocabulary
+//
+//  Created by jeremy thiriez on 17/07/2018.
+//  Copyright © 2018 jeremy thiriez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
